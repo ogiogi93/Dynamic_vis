8228c3f079e83df28a87aa8874b302fb60ddc60d
ogiogi93 <mail_ogiwara93@dx5.so-net.ne.jp>
2016-12-11 14:07:00 +0900

    状況名の変更

--- sql/semantic_table.sql
+++ sql/semantic_table.sql
@@ -11,13 +11,13 @@ CREATE TABLE semantic(
 );
 
 INSERT INTO semantic(situation,detail,num_of_people,stay_time_avg,firstvisit_rate,foreign_rate) VALUES
-('Danger', '訪日外国人などの慣れていない人々が多く長時間滞在している状況', 1, 1, 1, 1);
+('Complex', '訪日外国人などの慣れていない人々が多く長時間滞在している状況', 1, 1, 1, 1);
 INSERT INTO semantic(situation,detail,num_of_people,stay_time_avg,firstvisit_rate,foreign_rate) VALUES
 ('Event', '初めて訪問した人々が多くいる状況', 1, 0, 1, 0);
 INSERT INTO semantic(situation,detail,num_of_people,stay_time_avg,firstvisit_rate,foreign_rate) VALUES
-('confuse', '訪日外国人などの慣れていない人々が多く滞在している状況', 0, 0, 1, 1);
+('First Coming', '訪日外国人などの慣れていない人々が多く滞在している状況', 0, 0, 1, 1);
 INSERT INTO semantic(situation,detail,num_of_people,stay_time_avg,firstvisit_rate,foreign_rate) VALUES
-('congestion', '人々が多く滞在している状況', 1, 0, 0, 0);
+('Congestion', '人々が多く滞在している状況', 1, 0, 0, 0);
 INSERT INTO semantic(situation,detail,num_of_people,stay_time_avg,firstvisit_rate,foreign_rate) VALUES
 ('Stable', '平常状態', -1, -1, -1, -1);
 INSERT INTO semantic(situation,detail,num_of_people,stay_time_avg,firstvisit_rate,foreign_rate) VALUES
